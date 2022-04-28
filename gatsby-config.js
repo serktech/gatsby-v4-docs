2fa4b83e7e9c6ee09de33ece0a21dd6cab5889b7
Serhiy <serktech@gmail.com>
2022-04-28 14:21:52 -0400

    Added Query for Data with GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,36 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby4lab`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `Gatsby4lab`,
+        siteUrl: `https://www.example.com`
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+    plugins: [
+        "gatsby-plugin-image",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        "gatsby-plugin-mdx", "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+            "name": "images",
+            "path": "./src/images/"
+        },
+        __key: "images"
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "pages",
+                "path": "./src/pages/"
+            },
+            __key: "pages"
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: `blog`,
+                path: `${__dirname}/blog`,
+            }
+        },
+    ]
 };
--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout";
 const AboutPage = () => {
     return (
         <Layout pageTitle="About Page">
-            <p>I'm making this by following the Gatsby Tutorial.</p>
+            <p>This is about page.</p>
         </Layout>
     )
 }
