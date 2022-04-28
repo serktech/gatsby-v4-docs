5c17658a0b16b0a6d73b2f2780d4291f3c5e0fd0
Serhiy <serktech@gmail.com>
2022-04-28 13:34:35 -0400

    Use and Style React Components about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,12 @@
 // Step 1: Import React
 import * as React from 'react'
+import Layout from "../components/layout";
 // Step 2: Define your component
 const AboutPage = () => {
     return (
-        <main>
-            <title>About Me</title>
-            <h1>About Me</h1>
-            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-        </main>
+        <Layout pageTitle="About Page">
+            <p>I'm making this by following the Gatsby Tutorial.</p>
+        </Layout>
     )
 }
 // Step 3: Export your component
